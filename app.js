375d21612f34cf5dbea596846de69f6acd2a4524
tanzilamd <mdtanzil858@gmail.com>
2021-08-08 13:12:14 +0600

    all added

---
+++ app.js
@@ -0,0 +1,29 @@
+const name = document.querySelector('#name');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const tbody = document.querySelector('#tbody');
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (name.value == '' || author.value == '' || year.value == '') {
+        alert('Fill up all section')
+    }
+    else {
+    let newRow = document.createElement('tr');
+
+    let newName = document.createElement('th');
+    newName.innerHTML = name.value;
+    newRow.appendChild(newName)
+
+    let newAuthor = document.createElement('th');
+    newAuthor.innerHTML = author.value;
+    newRow.appendChild(newAuthor)
+
+    let newYear = document.createElement('th');
+    newYear.innerHTML = year.value;
+    newRow.appendChild(newYear)
+
+    tbody.appendChild(newRow)
+    }
+})
